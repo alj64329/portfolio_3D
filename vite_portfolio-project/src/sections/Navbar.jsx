7f4c50b53b75fbc2c7a65f0752fe74f8df05f081
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-06-11 23:55:17 -0700

    Issue with the nav bar

--- vite_portfolio-project/src/sections/Navbar.jsx
+++ vite_portfolio-project/src/sections/Navbar.jsx
@@ -22,8 +22,8 @@ const Navbar = ()=>{
     const toggleMenu =()=> setIsOpen((prevIsOpen)=>!prevIsOpen)
 
   return(
-    <heaader className='fixed top-0 left -0 right 0 z-50
-    bg-black/9-0'>
+    <header className='fixed top-0 left-0 right-0 z-50
+    bg-black/90'>
         <div className='max-w-7xl mx-auto'>
             <div className='flex justfy-between
             items-center py-5 mx-auto c-space' >
@@ -33,15 +33,22 @@ const Navbar = ()=>{
                 </a>
 
                 <button onClick= {toggleMenu} className='text-netural-400 hover: text-white
-                focus:outline-none sm: hidden flex' aria-lable ="Toggle menu">
+                focus:outline-none sm: hidden flex' aria-label ="Toggle menu">
                     <img src={isOpen? "assets/close.svg":"assets/menu.svg"} alt="toggle"
                     className='w-6 h-6' />
                 </button>
-                <nav className='sm:flex hidden'></nav>
-              <NavItems />
+
+                <nav className='sm:flex hidden'>
+                <NavItems />
+                </nav>
             </div>
+            <div className={`nav-sidebar ${isOpen? 'max-h-screen':'max-h-0'}`}>
+                <nav className='p-5'>
+                    <NavItems />
+                </nav>
             </div>
-            </heaader>
+        </div>
+    </header>
   )
 }
 export default Navbar
