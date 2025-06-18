a02ac8cc381963f2ffbd8ea006289e01cb771a5c
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-06-17 17:23:21 -0700

    3:22:39

--- vite_portfolio-project/src/App.jsx
+++ vite_portfolio-project/src/App.jsx
@@ -1,13 +1,15 @@
 import About from "./sections/About.jsx"
 import Hero from "./sections/Hero.jsx"
 import Navbar from "./sections/Navbar.jsx"
+import Project from "./sections/Project.jsx"
 
 const App = ()=>{
   return(
     <main className='max-w-7xl mx-auto'>
       <Navbar />
       <Hero /> 
-      <About />   
+      <About /> 
+      <Project />  
       </main>
   )
 }
